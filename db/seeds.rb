72cbb4b10fd818e69bec7d5945f052b350eab379
Xsaviox1 <savioxghoul@gmail.com>
2024-09-29 16:45:17 -0300

    banco de dados adicionado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,49 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user1 = User.create({
+    name: "Sávio",
+    role: "admin",
+    password: "12345"
+})
+
+user2 = User.create({
+    name: "Maria",
+    role: "player",
+    password: "abcde"
+})
+
+
+survey1 = Survey.create({
+    user: user1, 
+    titulo: "Pesquisa sobre Tecnologia",
+    amt_questions: 2,
+    status: "aberta"
+})
+
+survey2 = Survey.create({
+    user: user2, 
+    titulo: "Pesquisa sobre Música",
+    amt_questions: 3,
+    status: "fechada"
+})
+
+
+question1 = Question.create({
+    survey: survey1,  
+    content: "Qual sua linguagem de programação favorita?",
+    question_type: "aberta"
+})
+
+question2 = Question.create({
+    survey: survey1,  
+    content: "Você prefere front-end ou back-end?",
+    question_type: "escolha"
+})
+
+response1 = Response.create({
+    user: user2,  
+    content: "Eu gosto de Python"
+})
+
+response2 = Response.create({
+    user: user2,   
+    content: "Prefiro back-end"
+})
